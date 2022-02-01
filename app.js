b813834456821fb0addbe91ddd2a1622a36bb13f
Tanushgarg111 <98385876+Tanushgarg111@users.noreply.github.com>
2022-02-01 13:50:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const republic = document.getElementById("republic");
+
+republic.onclick=()=>{
+    //action 1
+ location.href="./republic.html";
+ 
+}
+
---
+++ table.js
@@ -0,0 +1,7 @@
+const table = document.getElementById("table");
+
+table.onclick=()=>{
+    //action 1
+ location.href="./table.html";
+ 
+}
